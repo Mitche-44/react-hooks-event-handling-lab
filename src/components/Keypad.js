b82c43d2c16963b399681f7946bec975ad8f1833
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-04-18 17:53:00 +0300

    add keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,22 @@
 // Code Keypad Component Here
 
 function Keypad (){
+ function handleChange(event) {
+    console.log("Entering password...");
+  }
+
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password" text="Entering password"  onChange={handleChange}/>
+
+
+
+            </form>
+
+
+
+        </div>
     )
 }
 
